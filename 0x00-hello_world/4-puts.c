b217c585342be23f61de72564c4e0cbbfec67942
Henry-Egboh <henryegboh@gmail.com>
2022-09-09 11:26:34 -0700

    printing using printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,11 @@
 /**
  * main - Entry point
  *
+ * Description: A program that prints a line of text with puts
+ *
  * Result: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
