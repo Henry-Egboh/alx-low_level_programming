12e0348517647ffbaadb9ea3aff84aa0698e24a8
Henry-Egboh <henryegboh@gmail.com>
2022-09-09 21:48:44 -0700

    Printing using write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print error
+ *
+ * Return: 1 (stderror)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
+}
