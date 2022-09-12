e333468486d43c6381dff7e19fe3837fefaf79d1
Henry-Egboh <henryegboh@gmail.com>
2022-09-12 05:32:19 -0700

    printing non-repetitive two digit numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Permutate two numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = '0'; x <= '9'; x++)
+{
+	for (y = x + 1; y <= '9'; y++)
+{
+	putchar(x);
+	putchar(y);
+	putchar(' ');
+}
+}
+	putchar('\n');
+	return (0);
+}
