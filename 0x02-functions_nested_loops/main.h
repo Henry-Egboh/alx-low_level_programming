691ab8967ff2c3b02f6cbd71e594ce25cc1bdf10
Henry-Egboh <henryegboh@gmail.com>
2022-09-13 07:07:51 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN
+#define MAIN
+/**
+ * _putchar - header file for printing
+ * @c: Take input and process
+ * Return: Always 1
+ *
+ * Author: Henry
+ * Description: Putchar function for output
+ */
+int _putchar(char c);
+#endif
