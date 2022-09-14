3a754f083224364f9577efec8fee3f0ed55fecf4
Henry-Egboh <henryegboh@gmail.com>
2022-09-13 17:43:11 -0700

    Print absolute numbers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Get absolute value
+ * @n: Take argument
+ * Return: Always n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+{
+	return (n);
+}
+	else
+{
+	return (-n);
+}
+}
