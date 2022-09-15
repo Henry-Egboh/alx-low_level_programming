8c98854f423803ebafe2b8ad8fa0ddb4b555b638
Henry-Egboh <henryegboh@gmail.com>
2022-09-15 15:00:27 -0700

    print twoo numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - mul two integers
+ * @a: integers
+ * @b: integers
+ * Return: Result
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
