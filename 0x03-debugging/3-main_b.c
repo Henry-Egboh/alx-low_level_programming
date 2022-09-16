dcc40c10a85ac917f6629f23339c0a373b5e7b16
Henry-Egboh <henryegboh@gmail.com>
2022-09-16 04:50:14 -0700

    printing dates

---
+++ 0x03-debugging/3-main_b.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Checks code
+ * Return: Always 0
+ */
+int main(void)
+{
+	int month;
+	int day;
+	int year;
+
+	month = 02;
+	day = 29;
+	year = 2000;
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+	day = convert_day(month, day);
+	print_remaining_days(month, day, year);
+	return (0);
+}
