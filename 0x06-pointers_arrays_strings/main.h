72c593617bafa38e8cb9368a4a6196d11e81cfab
Henry-Egboh <henryegboh@gmail.com>
2022-09-22 06:07:41 -0700

    Reverse an array

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *s);
 char *leet(char *s);
+void reverse_array(int *a, int n);
 char *_strcat(char *dest, char *src);
 #endif /*MAIN_H*/
