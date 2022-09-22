c6945eb29c6266fa727149f72cd2d8dacc99e89d
Henry-Egboh <henryegboh@gmail.com>
2022-09-22 10:52:42 -0700

    Print an integer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,6 @@ char *cap_string(char *s);
 char *leet(char *s);
 void reverse_array(int *a, int n);
 char *rot13(char *s);
+void print_number(int n);
 char *_strcat(char *dest, char *src);
 #endif /*MAIN_H*/
