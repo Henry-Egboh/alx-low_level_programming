56ba88bb0024b438c20ad661c1e4afe3879f66e9
Henry-Egboh <henryegboh@gmail.com>
2022-10-03 05:56:28 -0700

    print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - check
+ * @argc: size of an array
+ * @argv: array of pointers
+ * Return: Always 0
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
