e791f46c7defe3ac324d0b11f58625bc4f03eff2
Henry-Egboh <henryegboh@gmail.com>
2022-10-11 11:06:07 -0700

    Defined a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Description: dog that store values
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
