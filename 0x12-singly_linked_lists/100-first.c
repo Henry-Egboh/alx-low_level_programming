3d6c513948312547df1c91e346b33f4db2b4be37
Henry-Egboh <henryegboh@gmail.com>
2022-10-20 11:46:40 -0700

    print string before main function executes

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+/**
+ * cmain - print string
+ */
+void __attribute__ ((constructor)) cmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
